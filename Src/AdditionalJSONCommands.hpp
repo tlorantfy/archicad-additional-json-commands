bdd3874786bd018c148e5b587688081ba62b4835
Tibor Lorántfy <lorantfyt@gmail.com>
2021-05-20 22:31:16 +0200

    Delete unnecessary empty lines from code

--- Src/AdditionalJSONCommands.hpp
+++ Src/AdditionalJSONCommands.hpp
@@ -9,85 +9,65 @@
 
 class PublishCommand : public API_AddOnCommand {
 public:
-
 	virtual GS::String							GetName () const override;
 	virtual GS::String							GetNamespace () const override;
 	virtual GS::Optional<GS::UniString>			GetSchemaDefinitions () const override;
 	virtual GS::Optional<GS::UniString>			GetInputParametersSchema () const override;
 	virtual GS::Optional<GS::UniString>			GetResponseSchema () const override;
-
 	virtual API_AddOnCommandExecutionPolicy		GetExecutionPolicy () const override			{ return API_AddOnCommandExecutionPolicy::ScheduleForExecutionOnMainThread; }
-
 	virtual GS::ObjectState						Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
-
 	virtual void								OnResponseValidationFailed (const GS::ObjectState& response) const override;
 };
 
 
 class TeamworkReceiveCommand : public API_AddOnCommand {
 public:
-
 	virtual GS::String							GetName () const override;
 	virtual GS::String							GetNamespace () const override;
 	virtual GS::Optional<GS::UniString>			GetSchemaDefinitions () const override;
 	virtual GS::Optional<GS::UniString>			GetInputParametersSchema () const override;
 	virtual GS::Optional<GS::UniString>			GetResponseSchema () const override;
-
 	virtual API_AddOnCommandExecutionPolicy		GetExecutionPolicy () const override { return API_AddOnCommandExecutionPolicy::ScheduleForExecutionOnMainThread; }
-
 	virtual GS::ObjectState						Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
-
 	virtual void								OnResponseValidationFailed (const GS::ObjectState& response) const override;
 };
 
 
 class GetProjectInfoCommand : public API_AddOnCommand {
 public:
-
 	virtual GS::String							GetName () const override;
 	virtual GS::String							GetNamespace () const override;
 	virtual GS::Optional<GS::UniString>			GetSchemaDefinitions () const override;
 	virtual GS::Optional<GS::UniString>			GetInputParametersSchema () const override;
 	virtual GS::Optional<GS::UniString>			GetResponseSchema () const override;
-
 	virtual API_AddOnCommandExecutionPolicy		GetExecutionPolicy () const override { return API_AddOnCommandExecutionPolicy::ScheduleForExecutionOnMainThread; }
-
 	virtual GS::ObjectState						Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
-
 	virtual void								OnResponseValidationFailed (const GS::ObjectState& response) const override;
 };
 
 
 class GetArchicadLocationCommand : public API_AddOnCommand {
 public:
-
 	virtual GS::String							GetName () const override;
 	virtual GS::String							GetNamespace () const override;
 	virtual GS::Optional<GS::UniString>			GetSchemaDefinitions () const override;
 	virtual GS::Optional<GS::UniString>			GetInputParametersSchema () const override;
 	virtual GS::Optional<GS::UniString>			GetResponseSchema () const override;
-
 	virtual API_AddOnCommandExecutionPolicy		GetExecutionPolicy () const override { return API_AddOnCommandExecutionPolicy::ScheduleForExecutionOnMainThread; }
-
 	virtual GS::ObjectState						Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
-
 	virtual void								OnResponseValidationFailed (const GS::ObjectState& response) const override;
 };
 
 
 class QuitCommand : public API_AddOnCommand {
 public:
-
 	virtual GS::String							GetName () const override;
 	virtual GS::String							GetNamespace () const override;
 	virtual GS::Optional<GS::UniString>			GetSchemaDefinitions () const override;
 	virtual GS::Optional<GS::UniString>			GetInputParametersSchema () const override;
 	virtual GS::Optional<GS::UniString>			GetResponseSchema () const override;
-
 	virtual API_AddOnCommandExecutionPolicy		GetExecutionPolicy () const override { return API_AddOnCommandExecutionPolicy::ScheduleForExecutionOnMainThread; }
-
 	virtual GS::ObjectState						Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
-
 	virtual void								OnResponseValidationFailed (const GS::ObjectState& response) const override;
 };
 
